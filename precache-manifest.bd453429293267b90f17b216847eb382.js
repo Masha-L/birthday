b1598d320f5482f59164cbd1945ea80acb3fb6cb
Masha-L <lifsh22m@mtholyoke.edu>
2020-04-26 22:36:34 -0400

    Updates

---
+++ precache-manifest.bd453429293267b90f17b216847eb382.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6b2114ca893579b17d2c39ac3250e2",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "bcce4545422ca89c7fd3",
+    "url": "/birthday/static/css/main.a4c588e6.chunk.css"
+  },
+  {
+    "revision": "490abfc8b0fcd8c1fbc3",
+    "url": "/birthday/static/js/2.347b9301.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday/static/js/2.347b9301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcce4545422ca89c7fd3",
+    "url": "/birthday/static/js/main.b9d105ce.chunk.js"
+  },
+  {
+    "revision": "1e516489253baf857240",
+    "url": "/birthday/static/js/runtime-main.3f8b8146.js"
+  },
+  {
+    "revision": "0f9438036310b8064f413016c356e31f",
+    "url": "/birthday/static/media/Maya.0f943803.png"
+  },
+  {
+    "revision": "244735351632dd4c469fc4b8e429c253",
+    "url": "/birthday/static/media/alec.24473535.jpg"
+  },
+  {
+    "revision": "7fd4761fb8028792355fc6d2396dbe10",
+    "url": "/birthday/static/media/alec.7fd4761f.m4a"
+  },
+  {
+    "revision": "12fb405a39ad72e2d1094b8042272c65",
+    "url": "/birthday/static/media/arch.12fb405a.mov"
+  },
+  {
+    "revision": "630f4414bac33a32aeb72a7c674a4f16",
+    "url": "/birthday/static/media/lec.630f4414.mov"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/birthday/precache-manifest.43d1ebebf8bdf45cf83abf273eb89712.js"
+  "/birthday/precache-manifest.bd453429293267b90f17b216847eb382.js"
 );
 
 self.addEventListener('message', (event) => {
